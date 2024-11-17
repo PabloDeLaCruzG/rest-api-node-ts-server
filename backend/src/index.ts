a94559d2bc4f6d2753f86e5438082a64adb855e9
PabloMonjiDev <pablo20044814@gmail.com>
2024-11-17 22:24:04 +0100

    Removing frontend and backend en la rama pre-relocate

--- backend/src/index.ts
+++
@@ -1,7 +0,0 @@
-import colors from 'colors';
-import server from './server';
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(colors.magenta('Server is running on port ' + port));
-});
